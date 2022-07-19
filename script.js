130e85fe7e0c5fa58c74314a9f0a23d140ae89c2
Leximiller128 <leximiller128@gmail.com>
2022-07-19 14:32:43 -0600

    added score and adjusted timer

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ var startTitle = document.getElementById("start-title");
 var questionContainer = document.getElementById("question-container");
 var endGameEl = document.getElementById("end-game");
 var restartBtn = document.getElementById("restart-quiz");
+var resultsContainer = document.getElementById("results");
+var scoreEl = document.getElementById("score");
 
 //Game variables
 var currentQuestion = 0;
 var score = 0;
 var secondsRemaining = 75;
 var timer;
+var correctAnswer;
 
 //Questions array
 var questions = [
@@ -54,14 +57,16 @@ function startGame() {
   //hide start button by adding class 'hidden'
   startBtn.classList.add("hidden");
   startTitle.classList.add("hidden");
+  scoreEl.classList.remove("hidden");
+  scoreEl.textContent = `Score: ${score}`;
   //timer starts when quiz begins
   //show timer by removing class'hidden'
-  timerEl.textContent = secondsRemaining;
+  timerEl.textContent = `Time left: ${secondsRemaining}`;
   timerEl.classList.remove("hidden");
   //start the timer
   timer = setInterval(function () {
     secondsRemaining--;
-    timerEl.textContent = secondsRemaining;
+    timerEl.textContent = `Time left: ${secondsRemaining}`;
     if (secondsRemaining <= 0) {
       endGame();
     }
@@ -74,23 +79,39 @@ function startGame() {
 function showQuestion() {
   questionContainer.innerHTML = `
     <h2>${questions[currentQuestion].questionText}</h2>
-    <button class="option"data = "a">${questions[currentQuestion].options["a"]}</button>
-    <button class="option"data = "b">${questions[currentQuestion].options["b"]}</button>
-    <button class="option"data = "c">${questions[currentQuestion].options["c"]}</button>
-    <button class="option" data = "d">${questions[currentQuestion].options["d"]}</button>
+    <button class="option" data="a">${questions[currentQuestion].options["a"]}</button>
+    <button class="option" data="b">${questions[currentQuestion].options["b"]}</button>
+    <button class="option" data="c">${questions[currentQuestion].options["c"]}</button>
+    <button class="option" data="d">${questions[currentQuestion].options["d"]}</button>
   `;
 
   var optionBtns = document.querySelectorAll(".option");
-  console.log(optionBtns);
   //go to the next question
   for (let i = 0; i < optionBtns.length; i++) {
     optionBtns[i].addEventListener("click", function (e) {
-      let userAnswer = e.target.attributes.data.value;
-      //if the answer is correct then will move to current question
-      //else subtract 10 seconds and move on to next question
-      currentQuestion++;
-      //if answer is correct, go to the next question
-      if (currentQuestion < questions.length) {
+      const userAnswer = e.target.attributes.data.value;
+      const correctAnswer = questions[currentQuestion].correctAnswer;
+
+      console.log("user answer: ", userAnswer);
+      console.log("correct answer: ", correctAnswer);
+
+      if (userAnswer === correctAnswer) {
+        //add points to score
+        score += 15;
+        scoreEl.textContent = `Score: ${score}`;
+      } else {
+        secondsRemaining -= 15;
+        timerEl.textContent = `Timer: ${
+          secondsRemaining < 0 ? 0 : secondsRemaining
+        }`;
+        if (secondsRemaining <= 0) {
+          endGame();
+        }
+      }
+
+      //goes to next question
+      if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
         showQuestion();
       } else {
         endGame();
@@ -105,14 +126,23 @@ function endGame() {
   questionContainer.classList.add("hidden");
   endGameEl.classList.remove("hidden");
 }
+//marks correct answer
+for (let i = 0; i < correctAnswer; i++) {
+  optionBtns[i].addEventListener("click", function (e) {
+    let userAnswer = e.target.attributes.data.value;
+  });
+}
 
-restartBtn.addEventListener("click", start - title);
-function restartQuiz();
+//show scores
+function showResults() {
+  submitButton.addEventListener("click", showResults);
+}
+
+//restart quiz button
+// restartBtn.addEventListener("click", start - title);
+// function restartQuiz();
 
 //if the answer is incorrect, loose 15 seconds and go to the next question
-function subtract() {
-  subtract(timer);
-}
 
 // let subtract = timerEl.subtract(15);
 // console.log(format);
@@ -123,5 +153,3 @@ function subtract() {
 
 //at the end of the quiz
 //view high scores button
-
-//restart quiz button
